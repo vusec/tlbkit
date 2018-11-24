2cbff491adadd7e0489e2c8a147f9ab857cc9b72
Ben Gras <beng@shrike-systems.com>
2018-11-24 04:24:52 +0100

    gitignore

--- tlb-latency.py
+++ tlb-latency.py
@@ -46,7 +46,7 @@ def test_latency_and_miss_per_iteration(pagelist,event,name):
     xx,yy,ycycles=[],[],[]
     print 'calculating', event, 'and cycles slope for', name, 
     sys.stdout.flush()
-    for iterations in range(0,30000000,5000000):
+    for iterations in range(0,20000000,5000000):
                    pagelist_args = ['-D'+str(x) for x in pagelist]
                    cmdlist=["perf", "stat", "-x,", "-e", event+",cycles", "./c/tvlb-verbatim", "-r", str(iterations), "-c"+str(cpu)]+pagelist_args
                    print '.',
@@ -70,8 +70,28 @@ def test_latency_and_miss_per_iteration(pagelist,event,name):
     return event_per_iteration,cycles_per_iteration
 
 if __name__ == "__main__":
-    l1_setsize=4
-    l2_setsize=12
+    print ''
+    print ' * Probing set sizes'
+    print ''
+
+    for l1_size in range(1,10):
+        l1_pagelist_misses=tlblib.generate_set_l1(1,l1_size+1)
+        l1misses1,l1cycles1 = test_latency_and_miss_per_iteration(l1_pagelist_misses, 'dtlb_load_misses.stlb_hit', 'l1 miss test size ' + str(l1_size))
+        if l1misses1 > 1:
+            print 'Found'
+            print ''
+            break
+    for l2_size in range(7,16):
+        l2_pagelist_misses=tlblib.generate_set_l2_general(1,l2_size+1)
+        l2misses1,l2cycles1 = test_latency_and_miss_per_iteration(l2_pagelist_misses, 'dtlb_load_misses.miss_causes_a_walk', 'l2 miss test size ' + str(l2_size))
+        if l2misses1 > 1:
+            print 'Found'
+            print ''
+            break
+    print 'L1 size:', l1_size, 'L2 size:', l2_size
+
+    l1_setsize=l1_size
+    l2_setsize=l2_size
 
     print 'Assuming L1dTLB set size:', l1_setsize
     print 'Assuming L2dTLB/STLB set size:', l2_setsize
@@ -86,13 +106,17 @@ if __name__ == "__main__":
     l2_pagelist_no_misses[-1] += 1
     l2_pagelist_no_misses[-2] += 1
 
+    print ''
+    print ' * Configuring miss and no-miss TLB sets of equal size'
+    print ''
+
     print 'l1 misses:   ', l1_pagelist_misses
     print 'l1 no misses:', l1_pagelist_no_misses
     print 'l2 misses:   ', l2_pagelist_misses
     print 'l2 no misses:', l2_pagelist_no_misses
 
     print ''
-    print ' * Probing'
+    print ' * Probing miss rate and latency differences of miss and no-miss sets of equal size'
     print ''
 
     l1misses1,l1cycles1 = test_latency_and_miss_per_iteration(l1_pagelist_misses, 'dtlb_load_misses.stlb_hit', 'l1 miss list')
